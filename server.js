9ed1eb3c4d234b8d8e8de087044cb784ed0f7666
Jeff <modell.jeff@me.com>
2020-04-05 06:36:50 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv/config')
+const fastify = require("fastify")({ logger: true })
+const mongoose = require("mongoose")
+
+try {
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('MongoDB Connected Successfully.'))
+} catch(err) {
+    console.log(err)    
+}
+
+(async () => {
+    try {
+        await fastify.listen(9090)
+        fastify.log.info(`API is listening on ${fastify.server.address().port}`)
+    } catch(err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+})()
